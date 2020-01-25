786c6328f9b4318700d5fc2f47b3733b8ef083e4
Ethan Yu <yu920@purdue.edu>
2020-01-25 15:35:45 -0500

    First commit

---
+++ FoodFairy/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  FoodFairy
+//
+//  Created by Ethan Yu on 1/25/20.
+//  Copyright © 2020 Fairy Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    print("hi")
+
+    }
+
+
+}
+
