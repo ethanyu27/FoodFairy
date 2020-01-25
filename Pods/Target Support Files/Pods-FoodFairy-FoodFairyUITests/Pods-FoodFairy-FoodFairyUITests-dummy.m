0bbf7da014385a0c13ef6cd14ddc46621856ace9
Ethan Yu <yu920@purdue.edu>
2020-01-25 16:31:21 -0500

    Firebase/Pods Integration

---
+++ Pods/Target Support Files/Pods-FoodFairy-FoodFairyUITests/Pods-FoodFairy-FoodFairyUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FoodFairy_FoodFairyUITests : NSObject
+@end
+@implementation PodsDummy_Pods_FoodFairy_FoodFairyUITests
+@end
---
+++ Pods/Target Support Files/Pods-FoodFairyTests/Pods-FoodFairyTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FoodFairyTests : NSObject
+@end
+@implementation PodsDummy_Pods_FoodFairyTests
+@end
