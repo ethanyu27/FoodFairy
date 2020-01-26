3ee50f067c99ccffa2298186617333a8670d7e4f
sohanzaveri <sohanzaveri@gmail.com>
2020-01-26 01:44:08 -0500

    fdkals

--- FoodFairy/addOptionController.swift
+++ FoodFairy/addOptionController.swift
@@ -9,7 +9,7 @@
 import SwiftUI
 import FirebaseDatabase
 
-class addOptionController: UIViewController {
+class addOptionController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
    
     var ref: DatabaseReference!
     
@@ -23,7 +23,7 @@ class addOptionController: UIViewController {
     
     @IBOutlet weak var Food: UITextField!
     
-    @IBOutlet weak var Location: UITextField!
+    @IBOutlet weak var Location: UIPickerView!
     
     @IBOutlet weak var RoomNumber: UITextField!
     
@@ -32,11 +32,33 @@ class addOptionController: UIViewController {
     @IBOutlet weak var Description: UITextField!
     
     
+    let locations = ["ARMS", "BCC", "BRNG", "CL50", "CREC", "ELLT", "FORD", "FRNY", "HAMP", "HAWK", "HOVD", "KNOY", "KRAN", "KRCH", "LWSN", "MATH", "ME", "MSEE", "MTHW", "NUCL", "PHYS", "PMU", "POTR", "PSYC", "PUSH",  "RAWL", "REC", "SHRV",  "STEW", "WALC", "WILY", "WTHR", "YONG"]
+    
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return locations[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return locations.count
+    }
+
+var bldg = ""
+
+func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+            bldg = "\(row)"
+               }
+    
+    
+    
     @IBAction func Submit(_ sender: Any) {
-        
+                
         
         let food = Food.text
-        let bldg = Location.text
         let time = Time.text
         let description = Description.text
         let roomnum = RoomNumber.text
@@ -44,7 +66,7 @@ self.ref.child("Entries").childByAutoId().setValue([bldg, roomnum, food, time, d
 
         
         Food.text = ""
-        Location.text = ""
+        //Location.text = ""
         Time.text = ""
         Description.text = ""
         RoomNumber.text = ""
