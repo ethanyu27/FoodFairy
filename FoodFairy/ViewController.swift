f1f95ca5e0745a5a22dfa28bffd71e309b5cb2ce
gyolasan <yolasan4g@gmail.com>
2020-01-25 19:55:09 -0500

    map added

--- FoodFairy/ViewController.swift
+++ FoodFairy/ViewController.swift
@@ -7,16 +7,36 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, MKMapViewDelegate {
+    @IBOutlet weak var myMap: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        //let center = CLLocation(latitude: 40.423777, longitude: 86.921277)
+        // set initial location in Honolulu
+        let initialLocation = CLLocation(latitude: 21.282778, longitude: -157.829444)
+
+        centerMap(location: initialLocation)
         // Do any additional setup after loading the view.
+        
     print("Test")
 
     }
 
-
+    func centerMap(location: CLLocation) {
+        let radius = 1000
+        
+        let region = MKCoordinateRegion(center: location.coordinate, latitudinalMeters: CLLocationDistance(radius), longitudinalMeters: CLLocationDistance(radius))
+        myMap.setRegion(region, animated: true)
+        
+        //myMap.region = region
+    }
+    
+    func addAnnotationToMap(location: CLLocation) {
+        //let pin = MKAnnotation(location)
+        //let annotation = MKAnnotationView(annotation: <#T##MKAnnotation?#>, reuseIdentifier: <#T##String?#>)
+    }
 }
 
